b4ac2cf53cf546f087cc3a51d6169f037aa43461
researcher <researcher@autoquant>
2026-03-17 20:24:20 +0000

    BB breakout DI threshold 6 (was 8)

--- strategy.py
+++ strategy.py
@@ -76,7 +76,7 @@ def strategy(df: pd.DataFrame) -> pd.Series:
     signals[trend_up & (close < bb_lower)] = 1
 
     # BB upper breakout (momentum entry with smoothed DI)
-    signals[trend_up & (close > bb_upper) & (di_spread_smooth > 8)] = 1
+    signals[trend_up & (close > bb_upper) & (di_spread_smooth > 6)] = 1
 
     # Go flat during extreme volatility
     signals[extreme_vol] = 0
